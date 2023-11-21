1b03b37058e0ab622a0f6ea976f9aaa10bae31ac
catmajor <119273404+catmajor@users.noreply.github.com>
2023-11-21 17:25:21 +0000

    Basic express server setup

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 const express = require("express");
+const site = express();
+const fs = require("fs")
+;const port = 8443;
+
+site.get("/", (req, res) => {
+  res.setHeader("Content-Type", "text/html");
+  res.send("<h1>Hello World!</h1>");
+});
+
+site.listen(port);
