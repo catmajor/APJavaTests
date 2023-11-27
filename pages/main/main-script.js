92f630a658d00bea0553f17728565beea6187b9c
catmajor <119273404+catmajor@users.noreply.github.com>
2023-11-27 18:44:18 +0000

    RainDrop class for matrix style rain effect + Droplet class to be implemented later

--- pages/main/main-script.js
+++ pages/main/main-script.js
@@ -1,6 +1,6 @@
 function main () {
-  const availableChars = "!\"#$%&\'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~";
   function playFancyText() {
+    const availableChars = "!\"#$%&\'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~";
     function cycleString() {
       cycleTextArr = cycleTextArr.map(() => availableChars[Math.floor(Math.random()*availableChars.length)])
     }
@@ -14,13 +14,59 @@ function main () {
       cycleCount++;
       cycleString();
       fancyText.textContent = finalText.substring(0, startInd) + cycleTextArr.join("")
-      if (cycleCount%15===0) {
+      if (cycleCount%10===0) {
         startInd++;
         cycleTextArr.shift();
       }
     }, 20)
   }
+  function matrixRain() {
+    const availableChars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ01234567890123456789"
+    function RainDrop() {
+      if (this===window) throw "RainDrop must be called as a constructor";
+      function Droplet() {
+        if (this===window) throw "Droplet must be called as a constructor";
+        
+      }
+      let private = {};
+      private.top = 0;
+      private.size = Math.floor(Math.random()*20+20)
+      private.speed = Math.floor(Math.random()*10+10);
+      private.switchInterval = Math.floor(Math.random()*4+8);
+      private.frameCount = 0;
+      this.chars = new Array();
+      this.dom = document.createElement("div");
+      document.body.appendChild(this.dom);
+      this.dom.classList.add("matrix-rain");
+      this.dom.textContent = availableChars[Math.floor(Math.random()*availableChars.length)]
+      this.setTop = (px) => {
+        this.dom.style.top = `${px}px`;
+        private.top = px;
+      }
+      this.destructor = () => {
+        this.body.removeChild(this.dom);
+      }
+      this.callAction = () => {
+        this.setTop(private.top+private.speed);
+        private.frameCount++;
+        if (private.frameCount%private.switchInterval===0) {
+          this.dom.textContent = availableChars[Math.floor(Math.random()*availableChars.length)]
+        }
+        if (private.top + private.size >= window.innerHeight) {
+          this.destructor();
+        }
+      }
+    }
+    const rainDropArr = [];
+    rainDropArr.push(new RainDrop());
+    setInterval(() => {
+      rainDropArr.forEach((drop) => {
+        drop.callAction();
+      })
+    }, 100)
+  }
   playFancyText();
+  matrixRain();
 }
 
 main()
