19392a5b143d66f6354d0e0813fae70938349615
catmajor <119273404+catmajor@users.noreply.github.com>
2023-11-29 00:48:43 +0000

    Fix bugs, make rendering functions async, and make text bigger to lessen load

--- pages/main/main-script.js
+++ pages/main/main-script.js
@@ -32,7 +32,6 @@ function main () {
     document.body.appendChild(dom);
     const availableChars = "日ﾊﾐﾋｰｳｼﾅﾓﾆｻﾜﾂｵﾘｱﾎﾃﾏｹﾒｴｶｷﾑﾕﾗｾﾈｽﾀﾇﾍｦｲｸｺｿﾁﾄﾉﾌﾔﾖﾙﾚﾛﾝ012345789Z:・.\"=*+-<>¦çﾘｸ"
     function RainDrop(_startFrame) {
-      let rainDrop = this
       if (this===window) throw "RainDrop must be called as a constructor";
       function Droplet(parent) {
         if (this===window) throw "Droplet must be called as a constructor";
@@ -48,13 +47,20 @@ function main () {
         this.dom.style.fontSize = private.size;
         this.dom.style.animationDuration = `${private.fadeSpeed}s`
         this.potentialSwitchTimeout = null;
+        this.switchesTried = 0;
         this.potentialSwitch = () => {
           this.potentialSwitchTimeout = setTimeout(()=>{
             if (Math.floor(Math.random()*4)===0) {
               this.text.textContent = availableChars[Math.floor(Math.random()*availableChars.length)];
             }
+            this.switchesTried++;
+            if (this.switchesTried = 3) {
+              clearTimeout(this.potentialSwitchTimeout);
+              return
+            };
             this.potentialSwitch();
-          }, 1000); 
+            return;
+          }, 2000); 
         }
         this.potentialSwitch();
         setTimeout(() => {
@@ -65,12 +71,13 @@ function main () {
         }, private.fadeSpeed*1000)
       }
       let private = {};
-      private.size = Math.floor(Math.random()*20+20);
+      private.size = Math.floor(Math.random()*20+40);
       private.top = -2*private.size;
       private.left = Math.floor(Math.random()*window.innerWidth);
-      private.speed = Math.floor(Math.random()*10+10);
-      private.fadeSpeed = Math.floor(Math.random()*5+5)
+      private.speed = Math.floor(Math.random()*23+7);
+      private.fadeSpeed = Math.floor(Math.random()*10+10)
       private.framePrintInterval = Math.floor(private.size/private.speed);
+      if (private.framePrintInterval===0) private.framePrintInterval = 1;
       private.switchInterval = Math.floor(Math.random()*4+8);
       private.startFrame = _startFrame;
       private.dropletArr = [];
@@ -84,40 +91,54 @@ function main () {
       dom.appendChild(this.dom);
       this.dom.classList.add("matrix-rain-rainDrop");
       this.text.textContent = availableChars[Math.floor(Math.random()*availableChars.length)]
+      private.maxFall = (Math.floor(Math.random()*1.5)===0?window.innerHeight-Math.floor(Math.random()*window.innerHeight/2):window.innerHeight)-private.size;;
       this.setTop = (px) => {
         this.dom.style.top = `${px}px`;
         private.top = px;
       }
       this.destructor = () => {
         rainDropArr.splice(rainDropArr.indexOf(this), 1);
+        this.dom.removeChild(this.text);
         setTimeout(() => {
           dom.removeChild(this.dom);
           rainDrop = null;
           this.text = null;
           this.dom = null;
-          private.dropletArr = null;
+          private = null;
+          this.chars = null
         }, private.fadeSpeed*1000)
       }
-      this.callAction = (frame) => {
-        this.setTop(private.top+private.speed);
-        let frameSinceCreation = private.startFrame-frame;
-        if ((frameSinceCreation)%private.framePrintInterval===0) {
-          this.text.textContent = availableChars[Math.floor(Math.random()*availableChars.length)];
-          private.dropletArr.push(new Droplet(this));
+      this.callAction = async (frame) => {
+        try {
+          this.setTop(private.top+private.speed);
+          let frameSinceCreation = private.startFrame-frame;
+          if (frameSinceCreation%private.framePrintInterval===0) {
+            this.text.textContent = availableChars[Math.floor(Math.random()*availableChars.length)];
+            private.dropletArr.push(new Droplet(this));
+          }
+          if (private.top >= private.maxFall) {
+            this.destructor();
+          }
+        } catch (e) {
+          throw e;
         }
-        if (private.top + private.size >= window.innerHeight) {
-          this.destructor();
-        }
+        
       }
     }
     const rainDropArr = [];
     frame = 0;
+    rainDropArr.push(new RainDrop(frame))
+    rainDropArr.push(new RainDrop(frame))
+    rainDropArr.push(new RainDrop(frame))
+    rainDropArr.push(new RainDrop(frame))
+    let calculatedLengthChance = ((rainDropArr.length+1)*0.3)**2
     setInterval(() => {
-      rainDropArr.forEach((drop) => {
+      rainDropArr.forEach(async (drop) => {
         drop.callAction(frame);
       })
-      if (Math.floor(Math.random()*(rainDropArr.length))===0) {
+      if (Math.floor(Math.random()*calculatedLengthChance)===0) {
         rainDropArr.push(new RainDrop(frame))
+        calculatedLengthChance = ((rainDropArr.length+1)*0.3)**2
       }
       frame++;
     }, 100)
