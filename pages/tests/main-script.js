0c7fcc3c4a63876bac4c6ac4e14971a194dd2afa
catmajor <119273404+catmajor@users.noreply.github.com>
2023-11-29 19:25:16 +0000

    move code into central main function

--- pages/tests/main-script.js
+++ pages/tests/main-script.js
@@ -1,50 +1,53 @@
-const urlParams = new URLSearchParams(window.location.search);
-const indent = urlParams.get('indent') ?? false;
-const scanner = urlParams.get('scanner') ?? false;
-const comment = urlParams.get('comment') ?? false;
-const char = urlParams.get('char') ?? false;
+function main() {
+  const urlParams = new URLSearchParams(window.location.search);
+  const indent = urlParams.get('indent') ?? false;
+  const scanner = urlParams.get('scanner') ?? false;
+  const comment = urlParams.get('comment') ?? false;
+  const char = urlParams.get('char') ?? false;
 
 
-if (!(indent||scanner||comment||char)) {
-  alert("Please select a test");
-}
+  if (!(indent||scanner||comment||char)) {
+    alert("Please select a test");
+  }
 
-const checkboxesNodeList = document.querySelectorAll(".checkbox");
-const checkboxes = new Array(checkboxesNodeList.length);
+  const checkboxesNodeList = document.querySelectorAll(".checkbox");
+  const checkboxes = new Array(checkboxesNodeList.length);
 
-function TestList () {
-  this.indent = checkboxes[0];
-  this.scanner = checkboxes[1];
-  this.comment = checkboxes[2];
-  this.char = checkboxes[3];
-  this.tests = [this.indent, this.scanner, this.comment, this.char];
-  console.log(indent)
-  this.indent.setState(indent);
-  this.scanner.setState(scanner);
-  this.comment.setState(comment);
-  this.char.setState(char);
-}
+  function TestList () {
+    this.indent = checkboxes[0];
+    this.scanner = checkboxes[1];
+    this.comment = checkboxes[2];
+    this.char = checkboxes[3];
+    this.tests = [this.indent, this.scanner, this.comment, this.char];
+    console.log(indent)
+    this.indent.setState(indent);
+    this.scanner.setState(scanner);
+    this.comment.setState(comment);
+    this.char.setState(char);
+  }
 
-function checkBox (boxDom, ind) {
-  this.dom = boxDom;
-  this.state = false;
-  this.dom.addEventListener("mousedown", () => {
-    this.setState(!this.state);
-  });
-  this.setState = (state) => {
-    this.state = state;
-    if (state) {
-      this.dom.style.backgroundColor = "green";
-    } else {
-      this.dom.style.backgroundColor = "red";
+  function checkBox (boxDom, ind) {
+    this.dom = boxDom;
+    this.state = false;
+    this.dom.addEventListener("mousedown", () => {
+      this.setState(!this.state);
+    });
+    this.setState = (state) => {
+      this.state = state;
+      if (state) {
+        this.dom.style.backgroundColor = "green";
+      } else {
+        this.dom.style.backgroundColor = "red";
+      }
     }
   }
-}
-checkboxesNodeList.forEach((box, ind) => {
-  checkboxes[ind] = new checkBox(box, ind);
-})
+  checkboxesNodeList.forEach((box, ind) => {
+    checkboxes[ind] = new checkBox(box, ind);
+  })
 
-const testList = new TestList();
+  const testList = new TestList();
+}
+main();
 //alert(`${indent}, ${scanner}, ${comment}, ${char}`);
 
 //Indent, scanner, comment, char
