3788a9d822d83b8ee27d5e688b8046522898dcf2
catmajor <119273404+catmajor@users.noreply.github.com>
2023-11-29 18:13:01 +0000

    setup function for delivering page htmls, create tests page

--- index.js
+++ index.js
@@ -3,15 +3,31 @@ const site = express();
 const fs = require("fs");
 const port = 8443;
 
+const getPage = (dirName) => {
+  let html = fs.readFileSync(`./pages/${dirName}/main-index.html`, "utf-8");
+  let css = fs.readFileSync(`./pages/${dirName}/main-style.css`, "utf-8");
+  let js = fs.readFileSync(`./pages/${dirName}/main-script.js`, "utf-8");
+  html = html.replace("#$#THIS-WILL-BE-REPLACED-WITH-STYLE-CONTENT#$#", css);
+  html = html.replace("#$#THIS-WILL-BE-REPLACED-WITH-JS-CONTENT#$#", js);
+  return html;
+};
+
 site.get("/", (req, res) => {
   res.setHeader("Content-Type", "text/html");
-  let html = fs.readFileSync("./pages/main/main-index.html", "utf-8");
-  let css = fs.readFileSync("./pages/main/main-style.css", "utf-8");
-  let js = fs.readFileSync("./pages/main/main-script.js", "utf-8");
-  html = html.replace("#$#THIS-WILL-BE-REPLACED-WITH-STYLE-CONTENT#$#", css);
-  html = html.replace("#$#THIS-WILL-BE-REPLACED-WITH-JS-CONTENT#$#", js)
+  const html = getPage("main");
   res.send(html);
 });
+
+site.get("/api", (req, res) => {
+  res.setHeader("Content-Type", "text/html");
+  res.send("future api endpoint");
+});
+
+site.get("/tests", (req, res) => {
+  res.setHeader("Content-Type", "text/html");
+  const html = getPage("tests");
+  res.send(html);
+})
 
 site.listen(port, () => {
   console.log(`Listening on port ${port}`);
